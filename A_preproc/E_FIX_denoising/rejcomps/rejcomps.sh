169ffe6a98a246c65efdeb10c3c02a4f55c2304c
samiraepp <samira.epp@tum.de>
2020-06-11 14:12:08 +0200

    artifact decisions for training data

---
+++ A_preproc/E_FIX_denoising/rejcomps/rejcomps.sh
@@ -0,0 +1,30 @@
+echo	[1,2,3,4,9,10,13,15,16,17]	>>	BIK111_prerun1_rejcomps.txt
+echo	[10,11,12,13,14,15,16]	>>	BIK111_prerun2_rejcomps.txt
+echo	[1,2,3,4,5,7,9,10,11,12,14,15,16,17,18,19,20]	>>	BIK115_prerun1_rejcomps.txt
+echo	[1,2,3,4,6,7,9,13,14,15]	>>	BIK115_prerun2_rejcomps.txt
+echo	[3, 4, 5, 7, 11, 13, 14, 18]	>>	BIK118_prerun1_rejcomps.txt
+echo	[5, 9, 10, 11, 12, 14, 17, 19, 20]	>>	BIK118_prerun2_rejcomps.txt
+echo	[2, 4, 5, 7, 8, 9, 12, 14, 15, 16, 17, 18, 19, 20]	>>	BIK124_prerun1_rejcomps.txt
+echo	[1, 2, 4, 5, 10, 11, 14, 15, 18, 19, 20]	>>	BIK124_prerun2_rejcomps.txt
+echo	[3, 5, 9, 10, 11, 13, 14, 15, 16]	>>	BIK128_prerun1_rejcomps.txt
+echo	[1, 2, 3, 4, 5, 6, 7, 9, 10, 12, 17, 18, 19, 20, 21]	>>	BIK128_prerun2_rejcomps.txt
+echo	[5,6,8,10,14,15]	>>	BIK133_prerun1_rejcomps.txt
+echo	[2,5,7, 8,12,13,15]	>>	BIK133_prerun2_rejcomps.txt
+echo	[2,7,8,9,10,12,13,14]	>>	BIK205_prerun1_rejcomps.txt
+echo	[1,6,7,9,13,15,16]	>>	BIK205_prerun2_rejcomps.txt
+echo	[1, 2,4,5,7,13,17]	>>	BIK216_prerun1_rejcomps.txt
+echo	[1,3,4,5,6,11,14,15]	>>	BIK216_prerun2_rejcomps.txt
+echo	[3,6,8,10,12,17,18]	>>	BIK220_prerun1_rejcomps.txt
+echo	[7,8,9,10,12,14,16,18]	>>	BIK220_prerun2_rejcomps.txt
+echo	[1,4,7,10, 11]	>>	BIK225_prerun1_rejcomps.txt
+echo	[1,2,3,4,6,7,8,14]	>>	BIK225_prerun2_rejcomps.txt
+echo	[4, 12, 13, 14, 17, 18]	>>	BIK230_prerun1_rejcomps.txt
+echo	[5, 9, 10, 16, 17, 18, 19, 20]	>>	BIK230_prerun2_rejcomps.txt
+echo	[1, 2, 5, 6, 7, 10, 13, 15]	>>	BIK233_prerun1_rejcomps.txt
+echo	[1, 2, 3, 4, 5, 6, 9, 11, 13, 14, 15, 16]	>>	BIK233_prerun2_rejcomps.txt
+echo	[3, 6, 9, 10, 12, 13, 14, 15]	>>	BIK236_prerun1_rejcomps.txt
+echo	[1, 2, 4, 6, 10, 13]	>>	BIK236_prerun2_rejcomps.txt
+echo	[2, 4, 5, 7, 12, 16, 17, 18]	>>	BIK240_prerun1_rejcomps.txt
+echo	[3, 2, 4, 5, 6, 8, 10, 14, 16, 17]	>>	BIK240_prerun2_rejcomps.txt
+echo	[4, 6, 8, 16, 17]	>>	BIK245_prerun1_rejcomps.txt
+echo	[3, 4, 5, 6, 7, 9, 11, 12, 18]	>>	BIK245_prerun2_rejcomps.txt
